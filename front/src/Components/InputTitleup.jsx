af2cf31f8b97255f84b52b54ecdaaffadb93d5d7
NereusOcean <niki_5555@mail.ru>
2022-10-30 19:13:21 +0300

    Refactor add CssTransition

--- front/src/Components/InputTitleup.jsx
+++ front/src/Components/InputTitleup.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
 
-export const inputStyleTemplate = {
-    profile: "custom_input__profile",
-    login: "custom_input__log_in",
-    order: "custom_input__order",
-
-}
-
 function InputTitleup(props) {
+    const {
+        className,
+        placeholder,
+        refTo,
+        type,
+        pattern
+    } = props;
 
 
     return (
         <div className='custom_input'>
-            <div className={props.className} >
-                <p>{props.placeholder}</p>
-                <input ref={props.refTo} type={props.type} pattern={props.pattern} />
+            <div className={className} >
+                <p>{placeholder}</p>
+                <input ref={refTo} type={type} pattern={pattern} />
             </div>
         </div>
 
     )
 }
+
+InputTitleup.style = {
+    profile: "custom_input__profile",
+    login: "custom_input__log_in",
+    order: "custom_input__order",
+};
 
 export default InputTitleup
--- front/src/Pages/Main.jsx
+++ front/src/Pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Button, { buttonStyle } from '../Components/Button'
+import { useNavigate } from 'react-router-dom';
+
+import Button  from '../Components/Button'
 import CenterPage from '../Components/templateStyle/CenterPage'
-import Container, { containerStyleTEmplate } from '../Components/templateStyle/Container'
+import Container from '../Components/templateStyle/Container'
 import logout from '../Assets/img/logout.png'
 import avatar from '../Assets/img/Accountant.png'
-import { useNavigate } from 'react-router-dom';
+
 
 function Main({}) {
   const navigate = useNavigate();
@@ -19,12 +21,12 @@ function Main({}) {
 
   return (
     <CenterPage>
-      <Container className={containerStyleTEmplate.logout}>
+      <Container className={Container.style.logout}>
         <img src={avatar}></img>
         <h2>{name}</h2>
         <p>Должность: {position}</p>
         <p>График: {shift}</p>
-        <Button  onClick={logoutUser} className={buttonStyle.danger + 'button__fs20'} image={logout}>LogOut</Button>
+        <Button  onClick={logoutUser} className={Button.style.danger + 'button__fs20'} image={logout}>LogOut</Button>
       </Container>
     </CenterPage>
   )
