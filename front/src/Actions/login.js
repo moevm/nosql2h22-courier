048fa2db2735083a27c89b54dd5b6e1d71177058
Sergey Sorokumov <ssorokumov@gmail.com>
2022-11-01 18:08:14 +0300

    add localHost to request && addTestUser

--- front/src/Actions/login.js
+++ front/src/Actions/login.js
@@ -3,7 +3,7 @@ import {setUser} from "../Reducers/reducer/userReducer";
 
 export const Login =  (email, password) =>{
     return async dispatch =>{
-            let res = await axios.post('api/login',{
+            let res = await axios.post('http://127.0.0.1:5000/api/login',{
                 email,
                 password
             });
@@ -18,7 +18,7 @@ export const auth =  () =>{
     return async dispatch =>{
         try{
 
-            let res = await axios.get('api/auth',
+            let res = await axios.get('http://127.0.0.1:5000/api/auth',
                 {JWT:localStorage.getItem('token')}
             );
             dispatch(setUser(res.data.user));
