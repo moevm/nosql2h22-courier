72e138fac05ccafe3398effa4075c46d46763848
Sergey Sorokumov <ssorokumov@gmail.com>
2022-11-04 16:18:01 +0300

    save/load hot fix

--- back/db/save.sh
+++ back/db/save.sh
@@ -1,20 +1,3 @@
-FILE=users.json
-if [ -f "$FILE" ]; then
-    mongoexport -d courier -c users  -o users.json
-else
-    echo "$FILE does not exist."
-fi
-
-FILE=autopark.json
-if [ -f "$FILE" ]; then
-    mongoexport -d courier -c autopark -o autopark.json
-else
-    echo "$FILE does not exist."
-fi
-
-FILE=orders.json
-if [ -f "$FILE" ]; then
-    ongoexport -d courier -c orders  -o orders.json
-else
-    echo "$FILE does not exist."
-fi
+mongoexport -d courier -c users  -o users.json
+mongoexport -d courier -c autopark -o autopark.json
+mongoexport -d courier -c orders  -o orders.json
